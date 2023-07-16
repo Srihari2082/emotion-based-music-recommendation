ddc5303697644a152064e19c120aec2eb6b8e1f3
Srihari2082 <114753366+Srihari2082@users.noreply.github.com>
2023-07-16 11:10:19 +0530

    Update utils.py

--- Emotion-Music-Recommendation/utils.py
+++ Emotion-Music-Recommendation/utils.py
@@ -1,4 +1,4 @@
-''' Class for using separate thread for video streaming through web camera'''
+
 import cv2
 from threading import Thread
 class WebcamVideoStream:
@@ -9,22 +9,22 @@ class WebcamVideoStream:
 			self.stopped = False
 
 		def start(self):
-				# start the thread to read frames from the video stream
+				
 			Thread(target=self.update, args=()).start()
 			return self
 			
 		def update(self):
-			# keep looping infinitely until the thread is stopped
+		
 			while True:
-				# if the thread indicator variable is set, stop the thread
+			
 				if self.stopped:
 					return
-				# otherwise, read the next frame from the stream
+				
 				(self.grabbed, self.frame) = self.stream.read()
 
 		def read(self):
-			# return the frame most recently read
+		
 			return self.frame
 		def stop(self):
-			# indicate that the thread should be stopped
+		
 			self.stopped = True
